dac9472796cb4c0783e31bc332261ada13a705e0
Alexandru Miclea <alexandrumiclea@outlook.com>
2022-11-09 10:22:58 +0200

    Code Refactoring part 1

--- entropie_gen.py
+++ entropie_gen.py
@@ -1,15 +1,49 @@
-from itertools import combinations_with_replacement, product
-from operator import countOf
-import random
-from re import U
+from itertools import product
 
-with open("cuvinte_wordle.txt", "r") as wordle_list:
-    text = wordle_list.read()
-    cuvinte = list(map(list, text.split()))
+with open("cuvinte_wordle.txt", "r") as wordleList:
+    sAux = wordle_list.read()
+    lQueryWords = [str(x) for x in sAux.split()]
+wordleList.close()
+
+def read_permutation_file():
+    """
+    Reads the rez_query.txt file, in which 5 digits are written,
+    describing the wordle game's response to the last given query
+
+    The feedback is returned by the function as a list
+    """
+    with open("rez_query.txt", "r") as :
+        sAux = rez_query.read()
+        lPermutation = [int(x) for x in sAux.split()]
+
+    return lPermutation
+
+def generate_entropy_dictionary():
+    """
+    returns a dictionary where the key is a word and the value is the
+    information said word would return if chosen as a query
+    """
 
 permutari = []
 
-permutari = list(product('⬜🟨🟩', repeat=3))
+
+L = read_permutation_file()
+print(L)
+
+def wordInformation(word):
+
+    generateEntropyDictionary()
+
+    return
+
+    #TODO check the dictionary for words that can be valid queries
+    #based on the permutation returned by the wordle game
+
+    #TODO ceva
+
+
+
+permutari = list(product('\U00002B1C\U0001F7E8\U0001F7E9', repeat=5))
 #for x in cuvinte:
    # for i in range (243):
     #    for j in range (5):
@@ -17,4 +51,7 @@ permutari = list(product('⬜🟨🟩', repeat=3))
      #        permutari=aux%3
      #        aux=aux//3   
 print(permutari)
+print('\U00002B1C')
+print('\U0001F7E8')
+print('\U0001F7E9')
 #def temp1():
--- wordle.py
+++ wordle.py
@@ -1,17 +1,24 @@
-from operator import countOf
 import random
-from re import U
 
-with open("cuvinte_wordle.txt", "r") as wordle_list:
-    text = wordle_list.read()
-    cuvinte = list(map(str, text.split()))
-    #TODO de ce am folosit asta
+with open("cuvinte_wordle.txt", "r") as wordle_list_file:
+    text = wordle_list_file.read()
+    cuvinte = [str(x) for x in text.split()]
+wordle_list_file.close()
   
-    guess_word = random.choice(cuvinte)
+guess_word = random.choice(cuvinte)
+
+def writePermutationFile():
+    with open("rez_query.txt", "w") as rez_query:
+        for x in permutare_query:
+            rez_query.write(str(x) + ' ')
+    rez_query.close()
+
 
 guess_word = list(guess_word)
-#guess_word = ['S', 'I', 'U', 'U', 'U']
+print(guess_word)
 user_input = []
+L_user_inputs = []
+permutare_query = [2, 2, 2, 2, 2]
 
 cnt = 0
 
@@ -22,30 +29,42 @@ while user_input != guess_word:
     if s_user_input not in cuvinte:
         print("nu e query valid")
         continue
+    L_user_inputs.append(s_user_input)
     cnt += 1
     # litera pe aceeasi pozitie 🟩
     # litera nu e in pozitia corecta 🟨
     # litera nu exista ⬜
 
+    #array cu cifre bazat pe ce intoarce query-ul
+    # 0 -> aceeasi pozitie
+    # 1 -> undeva in cuvant
+    # 2 -> nu este
+    
+
     print(" ".join(user_input))
-    ok = 0
     
     for i in range(5):
         if user_input[i] == guess_word[i]:
+            permutare_query[i] = 0
             print('🟩', end='')
-        elif user_input[i] in guess_word and countOf(user_input, user_input[i]) <= countOf(guess_word, user_input[i]):
+        elif user_input[i] in guess_word:
+            permutare_query[i] = 1
             print('🟨', end='')
-        elif user_input[i] in guess_word and countOf(user_input, user_input[i]) > countOf(guess_word, user_input[i]):
-            if ok != countOf(user_input, user_input[i]) - countOf(guess_word, user_input[i]):
-               print('🟨', end='')
-               ok += 1
-            else:
-                print('⬜', end='')
         else:
+            permutare_query[i] = 2
             print('⬜', end='')
     print()
-        
+    writePermutationToFile()
 else: 
     print("Ai gasit") 
 
 print(f"Felicitari, dragule! Ai luat sfantul 5 la A$C, din {cnt} incercari.")
+print(f"Istoricul query-urilor tale este: {L_user_inputs}")
+
+with open("rezultate.txt", "w") as rezultate:
+    rezultate.write(s_guess_word + " ")
+    for x in L_user_inputs:
+        rezultate.write(str(x) + " ")
+    #rezultate.write(str(x) for x in L_user_inputs)
+
+
