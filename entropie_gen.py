91b29b57a235678a14feec8be8a1a189d7d7cde9
Alexandru Miclea <alexandrumiclea@outlook.com>
2022-11-12 22:19:13 +0200

    Small optimization changes
    Sub 18 secunde pt primele 100 de cuvinte nu scot

--- entropie_gen.py
+++ entropie_gen.py
@@ -25,11 +25,6 @@ def read_permutation_file():
 
     return lPermutation
 
-
-lPermutations = []
-
-
-
 L = read_permutation_file()
 lPermutations = list(product('012', repeat=5))
 #print(lPermutations[209])
@@ -42,11 +37,6 @@ def word_entropy(strargWord):
     for tPerm in lPermutations:
         sPossibleQueries = set(lQueryWords)
         var_word_information = 0.0
-        ok = 1
-        
-        #TODO check if permutation is valid before applying it
-        #i.e. if a word has 2 of 'A' and the query has an 'A', both are
-        #collored yellow
         
         """
 
@@ -54,52 +44,38 @@ def word_entropy(strargWord):
         for strWord in lQueryWords:
 
             for i in range(5):
-                for j in range(5):
-                    if (strargWord[i] == strargWord[j]):
-                        if (tPerm[i] == 1 or tPerm[i] == 2) and tPerm[j] == 0:
-                            ok = 0
-                            break
-            if ok:
-                for i in range(5):
-                    if int(tPerm[i]) == 2:
-                        if strargWord[i] != strWord[i]:
-                            break
-                    if int(tPerm[i]) == 0:
-                        if strargWord[i] in strWord:
-                            break
-                    if int(tPerm[i]) == 1:
-                        if (strargWord[i] not in strWord) or (strargWord[i] == strWord[i]):
-                            break
-                else:
-                    #break
-                    sPossibleQueries.add(strWord)
+                if int(tPerm[i]) == 2:
+                    if strargWord[i] != strWord[i]:
+                        break
+                if int(tPerm[i]) == 0:
+                    if strargWord[i] in strWord:
+                        break
+                if int(tPerm[i]) == 1:
+                    if (strargWord[i] not in strWord) or (strargWord[i] == strWord[i]):
+                        break
+            else:
+                #break
+                sPossibleQueries.add(strWord)
 
         """   
         #ALGORITM 2 
 
-        for i in range(5):
-                for j in range(5):
-                    if (strargWord[i] == strargWord[j]):
-                        if (tPerm[i] == 1 or tPerm[i] == 2) and tPerm[j] == 0:
-                            ok = 0
-                            break
-
-        if ok:
-            for i in range(5):
-                if int(tPerm[i]) == 2:
-                    #verde
-                    for strWord in lQueryWords:
-                        if strWord[i] != strargWord[i]:
-                            sPossibleQueries.discard(strWord)
-                if int(tPerm[i]) == 0:
-                    for strWord in lQueryWords:
-                        if strargWord[i] in strWord:
-                            sPossibleQueries.discard(strWord)
-                if int(tPerm[i]) == 1:
-                    for strWord in lQueryWords:
-                        if strargWord[i] not in strWord or strargWord[i] == strWord[i]:
-                            sPossibleQueries.discard(strWord)
         
+        for i in range(5):
+            if int(tPerm[i]) == 2:
+                #verde
+                for strWord in lQueryWords:
+                    if strWord[i] != strargWord[i]:
+                        sPossibleQueries.discard(strWord)
+            if int(tPerm[i]) == 0:
+                for strWord in lQueryWords:
+                    if strargWord[i] in strWord:
+                        sPossibleQueries.discard(strWord)
+            if int(tPerm[i]) == 1:
+                for strWord in lQueryWords:
+                    if strargWord[i] not in strWord or strargWord[i] == strWord[i]:
+                        sPossibleQueries.discard(strWord)
+    
         
         #print(len(sPossibleQueries))
         #print([str(x) for x in sPossibleQueries])
@@ -120,14 +96,6 @@ def word_entropy(strargWord):
     #print(finalEntropy)
     return finalEntropy
 
-def dictionary_entropy(arg):
-
-    #print(f"{arg} done!")
-    sWord = lQueryWords[arg]
-    return word_entropy(sWord)
-
-    #print(dic_entropty.items())
-
 
 # DOMNULE RUSU SUNTEM INTELIGENTI :))))))
 # (va rugam sa ne strangeti mana)
@@ -138,7 +106,7 @@ if __name__=="__main__":
     tStart = time.perf_counter()
     pool = multiprocessing.Pool(cpuCount)
     
-    dEntropy = pool.map(dictionary_entropy, range(0,len(lQueryWords)))
+    dEntropy = pool.map(word_entropy, lQueryWords[:100])
     pool.close()
     pool.join()
 
@@ -149,7 +117,7 @@ if __name__=="__main__":
     print(tStop - tStart)
     
     with open("entropie_cuvinte.txt", "w") as entropyDictionary:
-        for i in range(0, len(lQueryWords)):
+        for i in range(0, 100):
             entropyDictionary.write(lQueryWords[i] + ' ' + str(dEntropy[i]) + '\n')
 #print(word_entropy("HOBBY"))
 
